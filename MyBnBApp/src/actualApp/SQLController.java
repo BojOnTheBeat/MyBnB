46ebbfedf3518d9f63e71499952e4e4896b94274
Hon-Tat Brian Au <auhontat@cms-temp.utsc.utoronto.ca>
2016-07-24 17:17:09 -0400

    fixed multiple column issue

--- MyBnBApp/src/actualApp/SQLController.java
+++ MyBnBApp/src/actualApp/SQLController.java
@@ -871,7 +871,7 @@ public class SQLController {
 
 	public void search(String lati, String longi, String post_code, String addr, String city, String sort_by,
 			String price_min, String price_max, String after_date, String before_date, ArrayList<String> amenities) {
-		String query = "SELECT lid, type, laddr, postal_code, city, country, ldate, price FROM Listing, ListingAvailability";
+		String query = "SELECT Listing.lid, type, laddr, postal_code, city, country, ldate, price FROM Listing, ListingAvailability";
 		
 		// for amenity filter
 		for (String amenity: amenities) {
